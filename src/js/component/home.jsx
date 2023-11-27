b3e7dfb8008937e6cfdebd2f260ed1f72e211c43
Franco Reggiardo <138858381+FrancoRegg@users.noreply.github.com>
2023-11-27 15:16:40 +0000

    Estructura del componente contador

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,12 +1,19 @@
 import React from "react";
 
 
-
-//create your first component
-const Home = () => {
+const SimpleCounter = (props) => {
   return (
-		<h1>Contador</h1>
+		<div className="contador"> 
+    <h1>cronometro</h1>
+      <div className="icono">
+        <i class="fas fa-clock"></i>
+      </div>
+      <div className="four">{props.digitFour % 10}</div>
+      <div className="three">{props.digitThree % 10}</div>
+      <div className="two">{props.digitTwo % 10}</div>
+      <div className="one">{props.digitOne % 10}</div>
+    </div>
 	);
 };
 
-export default Home;
+export default SimpleCounter;
