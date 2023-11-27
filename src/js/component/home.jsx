57d43a8ac7e72ff3aa98d039e3fafa3439917ce1
Franco Reggiardo <138858381+FrancoRegg@users.noreply.github.com>
2023-11-27 15:57:36 +0000

    mponente

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -3,17 +3,19 @@ import React from "react";
 
 const SimpleCounter = (props) => {
   return (
-		<div className="contador"> 
-    <h1>cronometro</h1>
-      <div className="icono">
-        <i class="fas fa-clock"></i>
+    <div className="container">
+      <h1>segundero</h1>
+      <div className="contador">
+        <div className="icono">
+          <i className="fas fa-clock"></i>
+        </div>
+        <div className="four numero">{props.digitFour % 10}</div>
+        <div className="three numero">{props.digitThree % 10}</div>
+        <div className="two numero">{props.digitTwo % 10}</div>
+        <div className="one numero">{props.digitOne % 10}</div>
       </div>
-      <div className="four">{props.digitFour % 10}</div>
-      <div className="three">{props.digitThree % 10}</div>
-      <div className="two">{props.digitTwo % 10}</div>
-      <div className="one">{props.digitOne % 10}</div>
     </div>
-	);
+  );
 };
 
 export default SimpleCounter;
