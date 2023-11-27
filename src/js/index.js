7b3aad1ef7dccd647dbc41fc60b5f842db9190cb
Franco Reggiardo <138858381+FrancoRegg@users.noreply.github.com>
2023-11-27 15:16:12 +0000

    Funcionalidad basica del contador

--- src/js/index.js
+++ src/js/index.js
@@ -6,7 +6,20 @@ import ReactDOM from "react-dom";
 import "../styles/index.css";
 
 //import your own components
-import Home from "./component/home.jsx";
+import SimpleCounter from "./component/home.jsx";
 
 //render your react application
-ReactDOM.render(<Home />, document.querySelector("#app"));
+let counter = 0
+setInterval(function () {
+  const four = Math.floor(counter/1000)
+  const three = Math.floor(counter/100)
+  const two = Math.floor(counter/10)
+  const one = Math.floor(counter/1)
+
+  console.log(four, three, two, one);
+  counter++
+  ReactDOM.render(<SimpleCounter digitOne={one} digitTwo={two} digitThree={three} digitFour={four} />, document.querySelector("#app"));
+}, 1000)
+
+
+
